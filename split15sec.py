7390111fd0cc02776637c5a3bdd771e7f009704a
Detnarong Matham <dechnarong45628@gmail.com>
2023-09-15 21:01:25 +0700

    Update split15sec.py

--- split15sec.py
+++ split15sec.py
@@ -1,20 +1,49 @@
+import sys
 import pandas as pd
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QFileDialog
 
-# อ่านข้อมูลจาก CSV
-df = pd.read_csv('01-Guitar-Warm-Resampled.csv')
+class DataProcessorApp(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
 
-# แปลงคอลัมน์ 'LocalTimestamp' เป็น datetime
-df['LocalTimestamp'] = pd.to_datetime(df['LocalTimestamp'])
+    def initUI(self):
+        self.setGeometry(100, 100, 300, 200)
+        self.setWindowTitle('Data Processor')
+        
+        self.btn_open = QPushButton('Open CSV', self)
+        self.btn_open.setGeometry(50, 50, 200, 30)
+        self.btn_open.clicked.connect(self.openCSV)
 
-# กำหนดให้ 'LocalTimestamp' เป็น index
-df.set_index('LocalTimestamp', inplace=True)
+    def openCSV(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        fileName, _ = QFileDialog.getOpenFileName(self, "Open CSV File", "", "CSV Files (*.csv);;All Files (*)", options=options)
 
-# แบ่งข้อมูลทุก 15 วินาที
-interval = '15S'
-dfs_by_interval = [group for name, group in df.groupby(pd.Grouper(freq=interval))]
+        if fileName:
+            self.processCSV(fileName)
 
-# สร้างไฟล์ CSV สำหรับแต่ละช่วงเวลา 15 วินาที
-for i, df_interval in enumerate(dfs_by_interval):
-    filename = f'output_{i}.csv'
-    df_interval.to_csv(filename)
-    print(f'Saved {filename}')
+    def processCSV(self, filename):
+        # Read CSV
+        df = pd.read_csv(filename)
+
+        # Convert 'LocalTimestamp' column to datetime
+        df['LocalTimestamp'] = pd.to_datetime(df['LocalTimestamp'])
+
+        # Set 'LocalTimestamp' as index
+        df.set_index('LocalTimestamp', inplace=True)
+
+        # Split data into 15-second intervals
+        interval = '15S'
+        dfs_by_interval = [group for name, group in df.groupby(pd.Grouper(freq=interval))]
+
+        for i, df_interval in enumerate(dfs_by_interval):
+            output_filename = f'output_{i}.csv'
+            df_interval.to_csv(output_filename)
+            print(f'Saved {output_filename}')
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = DataProcessorApp()
+    window.show()
+    sys.exit(app.exec_())
